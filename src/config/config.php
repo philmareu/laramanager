e6ffa598f8b6a2fadaeca3cf65a5bc0552ccbf53
Phil Martinez <phil@philsquare.com>
2015-12-07 12:53:08 -0700

    updated package config and assets

--- src/config/config.php
+++ src/config/config.php
@@ -4,9 +4,40 @@ return [
 
     'site_title' => '',
 
-    'navigation' => [
-        [
+    'models_namespace' => 'App\\Models',
 
+    'home_uri' => 'admin',
+
+    'navigation' => [
+        'primary' => [
+            [
+                'title' => 'Users',
+                'uri' => 'users',
+                'icon' => 'uk-icon-users'
+            ]
+        ],
+        'shortcuts' => [
+            [
+                'title' => 'New User',
+                'uri' => 'admin/users/create',
+                'icon' => 'uk-icon-plus'
+            ]
+        ]
+    ],
+
+    'resources' => [
+        'users' => [
+            'title' => 'Users',
+            'model' => 'User',
+            'fields' => [
+                [
+                    'title' => 'Email',
+                    'name' => 'email',
+                    'type' => 'email',
+                    'validation' => 'required|email'
+                ]
+            ]
         ]
     ]
+
 ];
