8eaea302b881c45b7cfe0e414609a9ac1d3c0591
Phil Martinez <phil@philsquare.com>
2015-12-06 21:32:51 -0700

    styled login panel

--- src/views/auth/login.blade.php
+++ src/views/auth/login.blade.php
@@ -5,5 +5,50 @@
 @endsection
 
 @section('content')
-    @include('laraform::auth.login.admin', ['action' => url('admin/auth/login')])
+
+    <div class="uk-width-medium-1-5 uk-vertical-align-middle">
+        <div id="login-box">
+            <div class="title-bar"><i class="uk-icon-sign-in"></i> {{ config('laramanager.site_title') }} Admin</div>
+            <form class="uk-form uk-form-stacked uk-text-left" method="POST" action="{{ url('admin/auth/login') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="uk-form-row">
+                    <div class="uk-form-controls">
+                        <span class="errors uk-text-danger">{{ $errors->first('email') }}</span>
+                        <div class="uk-form-icon uk-form-controls">
+                            <i class="uk-icon-envelope"></i>
+                            <input type="text" placeholder="Email" name="email" class="uk-form-width-large" value="{{ old('email') }}">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="uk-form-row">
+                    <div class="uk-form-controls">
+                        <span class="errors uk-text-danger">{{ $errors->first('password') }}</span>
+                        <div class="uk-form-icon uk-form-controls">
+                            <i class="uk-icon-lock"></i>
+                            <input type="text" placeholder="Password" name="password" class="uk-form-width-large">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="uk-form-row">
+                    <input type="checkbox" value="1"> Remember
+                </div>
+
+                <div class="uk-form-row">
+                    <div class="uk-grid uk-grid-collapse uk-flex-middle">
+                        <div class="uk-width-medium-1-2">
+                            <button type="submit" class="uk-button uk-button-primary">Login</button>
+                        </div>
+                        <div class="uk-width-medium-1-2">
+                            <a href="{{ url('/password/email') }}" class="forgot-password">Forgot Password?</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <p class="uk-text-center">LaraManager by <a href="http://philsquare.com">Philsquare</a></p>
+    </div>
+
 @endsection
--- src/views/layouts/auth.blade.php
+++ src/views/layouts/auth.blade.php
@@ -7,11 +7,14 @@
 
     <title>{{ config('laramanager.site_title') }} | @yield('title')</title>
 
-    <link href="{{ asset("vendor/laramanager/styles.css") }}" rel="stylesheet" media="screen">
+    <link href="{{ asset("vendor/laramanager/css/styles.css") }}" rel="stylesheet" media="screen">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:300,500' rel='stylesheet' type='text/css'>
 
     @yield('head')
 </head>
-<body>
+<body class="auth uk-height-viewport uk-vertical-align uk-text-center">
 
 @yield('content')
 
