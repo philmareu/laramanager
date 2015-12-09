822f9bfcdb9e7ee382756306f93a8c8a20807cf5
Phil Martinez <phil@philsquare.com>
2015-12-09 11:09:38 -0700

    optionally change slug delimiter

--- src/views/resource/scripts/fields/slug.blade.php
+++ src/views/resource/scripts/fields/slug.blade.php
@@ -1,8 +1,11 @@
 <script>
     var target = "#" + "{{ $target }}";
     var slug = "#" + "{{ $id }}";
+    var type = "{{ $delimiter or "-" }}";
+
+    console.log(type);
 
     $(function() {
-        $(target).slugify({ slug: slug, type: "-" });
+        $(target).slugify({ slug: slug, type: type });
     });
 </script>
