eef21e80befc9718d8bccb04f3496adc7da98616
Phil Martinez <phil@philsquare.com>
2016-01-23 19:57:14 -0800

    reformatted gallery and updated create form

--- src/views/objects/photo_gallery/display.blade.php
+++ src/views/objects/photo_gallery/display.blade.php
@@ -1,7 +1,9 @@
-<div class="uk-grid">
-    <div class="uk-width-1-4">
-        @foreach($object->data('images') as $image)
-            <img src="{{ url('images/small/' . $image) }}" alt=""/>
-        @endforeach
-    </div>
+<div class="uk-margin-bottom" data-uk-grid>
+    @foreach($object->data('images') as $filename)
+        <div class="uk-width-1-2 uk-width-medium-1-3 uk-width-large-1-4">
+            <a href="{{ url('images/gallery-large/' . $filename) }}" data-uk-lightbox="{group:'post'}">
+                <img src="{{ url('images/post-gallery-small/' . $filename) }}" alt="">
+            </a>
+        </div>
+    @endforeach
 </div>
