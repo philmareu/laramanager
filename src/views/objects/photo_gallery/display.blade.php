e0ee91f26a1cd0da68adff832bb261e8104b98ce
Phil Martinez <phil@philsquare.com>
2016-01-24 15:57:52 -0800

    fixed zero state issue

--- src/views/objects/photo_gallery/display.blade.php
+++ src/views/objects/photo_gallery/display.blade.php
@@ -1,9 +1,17 @@
-<div class="uk-grid uk-margin-bottom" data-uk-grid>
-    @foreach($object->data('images') as $filename)
-        <div class="uk-width-1-2 uk-width-medium-1-3 uk-width-large-1-4 uk-margin-large-bottom">
-            <a href="{{ url('images/large/' . $filename) }}" data-uk-lightbox="{group:'post'}">
-                <img src="{{ url('images/small/' . $filename) }}" alt="">
-            </a>
-        </div>
-    @endforeach
-</div>
+@if($object->data('images') == "")
+
+    <p>No Images</p>
+
+@else
+
+    <div class="uk-grid uk-margin-bottom" data-uk-grid>
+        @foreach($object->data('images') as $filename)
+            <div class="uk-width-1-2 uk-width-medium-1-3 uk-width-large-1-4 uk-margin-large-bottom">
+                <a href="{{ url('images/large/' . $filename) }}" data-uk-lightbox="{group:'post'}">
+                    <img src="{{ url('images/small/' . $filename) }}" alt="">
+                </a>
+            </div>
+        @endforeach
+    </div>
+
+@endif
--- src/views/objects/photo_gallery/edit.blade.php
+++ src/views/objects/photo_gallery/edit.blade.php
@@ -7,15 +7,23 @@
         Drag files here or <a class="uk-form-file">selecting one<input id="upload-select" type="file"></a>. (10Mb Max)
     </div>
 
-    <div id="file-gallery" class="uk-grid">
-        @foreach($object->data('images') as $image)
-            <div class="file">
-                <img src="{{ url('images/small/' . $image) }}" alt=""/>
-                <input type="hidden" name="data[images][]" value="{{ $image }}">
-                <button class="delete-file">Delete</button>
-            </div>
-        @endforeach
-    </div>
+    @if($object->data('images') == "")
+
+        No images
+
+    @else
+
+        <div id="file-gallery" class="uk-grid">
+            @foreach($object->data('images') as $image)
+                <div class="file">
+                    <img src="{{ url('images/small/' . $image) }}" alt=""/>
+                    <input type="hidden" name="data[images][]" value="{{ $image }}">
+                    <button class="delete-file">Delete</button>
+                </div>
+            @endforeach
+        </div>
+
+    @endif
 
     <div id="progressbar" class="uk-progress uk-hidden">
         <div class="uk-progress-bar" style="width: 0%;">...</div>
