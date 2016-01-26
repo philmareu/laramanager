64b5a567024ba8ff472d14d787e07c3c564b9819
Phil Martinez <phil@philsquare.com>
2016-01-26 11:38:55 -0800

    added method to oject for object file retrieval

--- src/views/browser/file.blade.php
+++ src/views/browser/file.blade.php
@@ -1,3 +1,6 @@
 <div class="uk-width-1-2 uk-width-medium-1-4 uk-width-large-1-6 uk-margin-bottom">
-    <img src="{{ url('images/medium/' . $file->filename) }}" alt="" data-laramanager-file-id="{{ $file->id }}" class="select-image">
+    <img src="{{ url('images/medium/' . $file->filename) }}" alt=""
+         data-laramanager-file-id="{{ $file->id }}"
+         data-laramanager-filename="{{ $file->filename }}"
+         class="select-image">
 </div>
