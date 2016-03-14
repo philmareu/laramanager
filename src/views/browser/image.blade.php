25fed14151c13433f8d49dee1fdbf99dded7c6fd
Phil Martinez <phil@philsquare.com>
2016-03-14 15:24:57 -0700

    updated image browser gallery with thumbnails and visible alt text

--- src/views/browser/image.blade.php
+++ src/views/browser/image.blade.php
@@ -1,6 +1,17 @@
 @if(isset($image))
-    <img src="{{ url('images/image-browser/' . $image->filename) }}" alt="{{ $image->alt }}"
-         data-laramanager-image-id="{{ $image->id }}"
-         data-laramanager-filename="{{ $image->filename }}"
-         class="unselected-image {{ $image->alt == '' ? 'needs-alt' : '' }}">
+    <div>
+        <div class="uk-thumbnail">
+            <img src="{{ url('images/image-browser/' . $image->filename) }}" alt="{{ $image->alt }}"
+                 data-laramanager-image-id="{{ $image->id }}"
+                 data-laramanager-filename="{{ $image->filename }}"
+                 class="unselected-image">
+            <div class="uk-thumbnail-caption">
+                @if($image->alt == '')
+                    <span class="uk-text-danger">No Alt Text</span>
+                @else
+                    {{ $image->alt }}
+                @endif
+            </div>
+        </div>
+    </div>
 @endif
