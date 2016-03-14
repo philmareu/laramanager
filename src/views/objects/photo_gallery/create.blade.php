f9882739849c53fb69f658467f687b7ec75e9cb4
Phil Martinez <phil@philsquare.com>
2016-03-14 11:47:16 -0700

    created images object field and update photo gallery

--- src/views/objects/photo_gallery/create.blade.php
+++ src/views/objects/photo_gallery/create.blade.php
@@ -2,17 +2,6 @@
 
 @section('form')
 
-    <div class="uk-form-row field-images">
-
-        <label for="data[photos]" class="uk-form-label">Photos</label>
-
-        <div class="uk-placeholder {{ $errors->has('data[file_ids]') ? 'uk-form-danger' : '' }}">
-            <div class="uk-grid uk-grid-small uk-sortable images-container" data-uk-sortable>
-            </div>
-        </div>
-
-        <input type="hidden" name="images_field_name" value="data[file_ids]">
-        <button type="button" class="uk-button opens-image-browser" data-limit="3"><i class="uk-icon-photo"></i> Browse</button>
-    </div>
+    @include('laramanager::objects.fields.images', ['name' => 'file_ids', 'label' => 'Photos'])
 
 @endsection
