52a2260376d8d7c2b516189cec1b8850a78396ec
Phil Martinez <phil@philsquare.com>
2016-05-03 07:47:44 -0700

    added missing User contracts

--- src/Philsquare/LaraManager/Models/User.php
+++ src/Philsquare/LaraManager/Models/User.php
@@ -1,8 +1,16 @@
 <?php namespace Philsquare\LaraManager\Models; 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
-class User extends Model {
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword;
 
     protected $fillable = [
         'name',
