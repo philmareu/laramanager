fb60b8b8f4fb23954870fe533a5845a44b908a49
Phil Martinez <phil@philsquare.com>
2016-05-03 21:18:44 -0700

    updated doc blocks in image controller

--- src/Philsquare/LaraManager/Http/Controllers/ImagesController.php
+++ src/Philsquare/LaraManager/Http/Controllers/ImagesController.php
@@ -11,16 +11,40 @@ use Philsquare\LaraManager\Repositories\ImageRepository;
 
 class ImagesController extends Controller {
 
+    /**
+     * The Image Repository
+     *
+     * @var ImageRepository
+     */
     protected $imageRepository;
 
+    /**
+     * The Uploader handles uploading of files
+     *
+     * @var Uploader
+     */
     protected $uploader;
 
+    /**
+     * Create a new images controller instance
+     *
+     * @param ImageRepository $imageRepository
+     * @param Uploader $uploader
+     */
     public function __construct(ImageRepository $imageRepository, Uploader $uploader)
     {
         $this->imageRepository = $imageRepository;
         $this->uploader = $uploader;
     }
 
+    /**
+     * This provides the gallery of images
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\JsonResponse|\Illuminate\View\View
+     * @throws \Exception
+     * @throws \Throwable
+     */
     public function index(Request $request)
     {
         $images = $this->imageRepository->getPaginated();
@@ -32,6 +56,14 @@ class ImagesController extends Controller {
         return view('laramanager::images.index', compact('images'));
     }
 
+    /**
+     * Display edit image form
+     *
+     * @param $imageId
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     * @throws \Throwable
+     */
     public function edit($imageId)
     {
         $image = $this->imageRepository->getById($imageId);
@@ -43,11 +75,26 @@ class ImagesController extends Controller {
         ]);
     }
 
+    /**
+     * Update image from form
+     *
+     * @param UpdateImageRequest $request
+     * @param $imageId
+     * @return mixed
+     */
     public function update(UpdateImageRequest $request, $imageId)
     {
         return $this->imageRepository->update($imageId, $request->all());
     }
 
+    /**
+     * Returns images from search results
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\JsonResponse|\Illuminate\View\View
+     * @throws \Exception
+     * @throws \Throwable
+     */
     public function search(Request $request)
     {
         $images = $this->imageRepository->search($request->term);
@@ -59,6 +106,12 @@ class ImagesController extends Controller {
         return view('laramanager::images.index', compact('images'));
     }
 
+    /**
+     * Show image browser in the WYSIWYG editor
+     *
+     * @param Request $request
+     * @return mixed
+     */
     public function imageBrowser(Request $request)
     {
         return view('laramanager::browser.wysiwyg')
@@ -66,6 +119,14 @@ class ImagesController extends Controller {
             ->with('images', $this->imageRepository->getPaginated());
     }
 
+    /**
+     * Upload and save image information
+     *
+     * @param UploadImageRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     * @throws \Throwable
+     */
     public function upload(UploadImageRequest $request)
     {
         $upload = $this->uploader->upload($request->file('image'), storage_path('app/laramanager/images'));
@@ -78,6 +139,8 @@ class ImagesController extends Controller {
     }
 
     /**
+     * Helper for converting array data into json responses
+     *
      * @param $output
      * @return \Illuminate\Http\JsonResponse
      */
