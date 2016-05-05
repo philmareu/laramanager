debbe4357c2d01775c06fce661eba0c9d152224b
Phil Martinez <phil@philsquare.com>
2016-05-05 10:47:09 -0700

    where da icons

--- src/views/resources/create.blade.php
+++ src/views/resources/create.blade.php
@@ -22,7 +22,7 @@
         @include('laramanager::partials.elements.form.text', ['field' => ['name' => 'model', 'value' => 'Models\\']])
         @include('laramanager::partials.elements.form.text', ['field' => ['name' => 'order_column', 'value' => 0]])
         @include('laramanager::partials.elements.form.select', ['field' => ['name' => 'order_direction', 'options' => ['asc' => 'asc', 'desc' => 'desc']]])
-        @include('laramanager::partials.elements.form.text', ['field' => ['name' => 'icon', 'value' => 'uk-icon-']])
+        @include('laramanager::partials.elements.form.text', ['field' => ['name' => 'icon', 'value' => 'uk-icon-', 'label' => 'Icon (http://getuikit.com/docs/icon.html)']])
 
         <div class="uk-form-row">
             <button type="submit" class="uk-button uk-button-primary uk-width-1-1 uk-width-medium-1-3 uk-width-large-1-6">Save</button>
