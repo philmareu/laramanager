20b4ec78cc568ef5ea4c12db857360f5c05f4238
Phil Mareu <philmareu@gmail.com>
2018-02-21 10:36:59 -0800

    we can now use the laravel config for app name

--- src/views/layouts/default.blade.php
+++ src/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $laraManagerSettings['site-name'] }} Admin | @yield('title')</title>
+    <title>{{ config('app.name') }} Admin | @yield('title')</title>
 
     <link href="{{ asset("vendor/laramanager/css/styles.css") }}" rel="stylesheet" media="screen">
 
