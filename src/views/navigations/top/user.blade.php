0c83002634a289e6aba3d47294f4d3979d7ed7b7
Phil Mareu <philmareu@gmail.com>
2018-02-21 09:39:00 -0800

    cleaned up logout link

--- src/views/navigations/top/user.blade.php
+++ src/views/navigations/top/user.blade.php
@@ -3,11 +3,9 @@
     <div class="uk-navbar-dropdown" uk-dropdown="mode: click">
         <ul class="uk-nav uk-navbar-dropdown-nav">
             <li>
-                <a href="{{ url('/admin/logout') }}"
+                <a href="#"
                    onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
+                   document.getElementById('logout-form').submit();">Logout</a>
 
                 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                     {{ csrf_field() }}
