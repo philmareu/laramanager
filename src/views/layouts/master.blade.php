a5664774b0e04c7481876dd4f5ccadd793eae656
Phil Mareu <philmareu@gmail.com>
2018-02-21 18:37:44 -0800

    added csrf token in head

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }} Admin | @yield('title')</title>
 
@@ -15,6 +16,7 @@
     <script type="text/javascript" charset="utf-8">
         var SITE_URL = "{{ url('') }}";
         var csrf = "{{ csrf_token() }}";
+        window.Laravel = {!! json_encode(['csrfToken' => csrf_token()]) !!};
     </script>
 
     <!-- Scripts -->
