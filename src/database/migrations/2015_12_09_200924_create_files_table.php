df74a0de2d739c4a9fe28cbcd180ef9f3e092921
Phil Mareu <philmareu@gmail.com>
2018-02-22 20:14:16 -0800

    description can be blank

--- src/database/migrations/2015_12_09_200924_create_files_table.php
+++ src/database/migrations/2015_12_09_200924_create_files_table.php
@@ -16,7 +16,7 @@ class CreateFilesTable extends Migration
             $table->increments('id');
             $table->string('filename', 110);
             $table->string('title');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->string('original_filename');
             $table->string('alt');
             $table->unsignedInteger('size')->nullable();
