614eb0452cf8fccae8569e4c9217f63de45413d4
Phil Mareu <philmareu@gmail.com>
2018-02-23 15:03:32 -0800

    added ability to remove images

--- src/assets/js/vue/components/ImagesField.vue
+++ src/assets/js/vue/components/ImagesField.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div v-for="image in images">
+    <div class="uk-child-width-1-4@s" uk-grid>
+        <div v-for="image in images" @click="removeImage(image)">
             <img :src="imageUrl('image-browser', image.filename)" alt="">
             <input type="hidden" :name="field.slug" v-model="image.id">
         </div>
@@ -30,6 +30,11 @@
             },
             imageUrl: function (filter, filename) {
                 return SITE_URL + '/images/' + filter + '/' + filename;
+            },
+            removeImage: function (image) {
+                this.images = _.filter(this.images, function(item) {
+                    return item.id !== image.id;
+                });
             }
         },
 
