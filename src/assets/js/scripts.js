f25d308aaff81d9c5dee7ca84bc6da39c017aaf5
Phil Mareu <philmareu@gmail.com>
2018-02-23 10:28:25 -0800

    updated date field to use new library

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -20,6 +20,8 @@ import Icons from 'uikit/dist/js/uikit-icons';
 
 UIkit.use(Icons);
 
+window.datepicker = require('@fengyuanchen/datepicker');
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
