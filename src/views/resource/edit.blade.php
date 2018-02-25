0731f7599f7a5ed455a905f59c63420874316c91
Phil Mareu <philmareu@gmail.com>
2018-02-25 11:33:32 -0800

    added image browser to edit screen

--- src/views/resource/edit.blade.php
+++ src/views/resource/edit.blade.php
@@ -6,6 +6,8 @@
 
 @section('page-content')
 
+    <image-browser-modal v-on:image-selected="setSelectedImage"></image-browser-modal>
+
     <form action="{{ route('admin.' . $resource->slug . '.update', $entity->id) }}" enctype="multipart/form-data" method="POST" class="uk-form uk-form-stacked">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="hidden" name="_method" value="PUT">
