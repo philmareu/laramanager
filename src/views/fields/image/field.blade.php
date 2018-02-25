2ec509de6f7f0bdf75ba280ae5508bca5f79cf97
Phil Mareu <philmareu@gmail.com>
2018-02-25 11:31:41 -0800

    removed old code left for reference

--- src/views/fields/image/field.blade.php
+++ src/views/fields/image/field.blade.php
@@ -1,25 +1,3 @@
-{{--@inject('image', 'Philsquare\LaraManager\Models\LaramanagerImage')--}}
-
-{{--<div class="uk-form-row field-images">--}}
-    {{--<label for="{{ $field->slug }}" class="uk-form-label">{{ $field->title }}</label>--}}
-
-    {{--<div class="uk-placeholder {{ $errors->has($field->slug) ? 'uk-form-danger' : '' }}">--}}
-        {{--<div class="uk-grid uk-grid-small uk-sortable images-container" data-uk-sortable>--}}
-
-            {{--@if(null !== old($field->slug))--}}
-                {{--@include('laramanager::browser.image', ['image' => $image->find(old($field->slug))])--}}
-            {{--@elseif(isset($entity) && $entity->{$field->data['method']})--}}
-                {{--@include('laramanager::browser.image', ['image' => $entity->{$field->data['method']}])--}}
-            {{--@endif--}}
-
-        {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<input type="hidden" name="{{ $field->slug }}" value="{{ $entity->{$field->slug} or ''}}" id="{{ $field->id }}" class="file_id">--}}
-    {{--<button type="button" class="uk-button opens-image-browser" data-limit="1"><i class="uk-icon-photo"></i> Browse</button>--}}
-
-{{--</div>--}}
-
 <image-field :field="{{ $field }}"
              :selected-image="selectedImage"
              :active-field="activeField"
--- src/views/fields/images/field.blade.php
+++ src/views/fields/images/field.blade.php
@@ -1,28 +1,3 @@
-{{--@inject('image', 'Philsquare\LaraManager\Models\LaramanagerImage')--}}
-{{--<div class="uk-form-row field-images">--}}
-
-    {{--<label for="{{ $field->slug }}" class="uk-form-label">{{ $field->title }}</label>--}}
-
-    {{--<div class="uk-placeholder {{ $errors->has($field->slug) ? 'uk-form-danger' : '' }}">--}}
-        {{--<div class="uk-grid uk-grid-small uk-sortable images-container" data-uk-sortable>--}}
-
-            {{--@if(null !== old($field->slug))--}}
-                {{--@foreach(old($field->slug) as $imageId)--}}
-                    {{--@include('laramanager::fields.images.image', ['image' => $image->find($imageId)])--}}
-                {{--@endforeach--}}
-            {{--@elseif(isset($entity) && $entity->{$field->data['method']}->count())--}}
-                {{--@foreach($entity->{$field->data['method']} as $image)--}}
-                    {{--@include('laramanager::fields.images.image', ['image' => $image])--}}
-                {{--@endforeach--}}
-            {{--@endif--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<input type="hidden" name="images_field_name" value="{{ $field->slug }}">--}}
-    {{--<button type="button" class="uk-button opens-image-browser" data-limit="3"><i class="uk-icon-photo"></i> Browse</button>--}}
-{{--</div>--}}
-
-
 <images-field :field="{{ $field }}"
              :selected-image="selectedImage"
              :active-field="activeField":errors="{{ $errors }}"
