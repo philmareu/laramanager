998237a0e416614f25257c9e6ab72a808074df74
Phil Mareu <philmareu@gmail.com>
2018-02-27 09:45:46 -0800

    updated nav style on small screens

--- src/views/navigations/primary/index.blade.php
+++ src/views/navigations/primary/index.blade.php
@@ -1,10 +1,10 @@
 @include('laramanager::navigations.primary.header')
 
-<ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
+<ul class="uk-nav uk-nav-default uk-nav-parent-icon" uk-nav>
     @include('laramanager::navigations.primary.items')
 </ul>
 
-<div id="offcanvas-navigation" uk-offcanvas>
+<div id="offcanvas-navigation" class="background-gradient-primary" uk-offcanvas>
     <div class="uk-offcanvas-bar">
         @include('laramanager::navigations.primary.header')
 
