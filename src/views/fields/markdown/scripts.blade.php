407e9d079f9bc8ef13be33074c117e59dedfe6ea
Phil Mareu <philmareu@gmail.com>
2018-07-15 13:00:12 -0500

    initial and focus editor on modal open

--- src/views/fields/markdown/scripts.blade.php
+++ src/views/fields/markdown/scripts.blade.php
@@ -1,17 +1,17 @@
 @push('scripts-last')
 <script>
-    let editor = codemirror.fromTextArea(document.getElementById('markdown-{{ $field->id }}'), {
-        mode: 'gfm',
-        theme: "default"
-    })
+    UIkit.util.on('#modal-markdown-{{ $field->id }}', 'show', function () {
+        let editor = codemirror.fromTextArea(document.getElementById('markdown-{{ $field->id }}'), {
+            mode: 'gfm',
+            theme: "default",
+        })
 
-    editor.on('change', function(event) {
-        document.getElementById('parsed-markdown-{{ $field->id }}').innerHTML = marked(editor.getValue());
-        document.getElementById('markdown-value-{{ $field->id }}').value = editor.getValue();
-    })
+        editor.focus()
 
-    UIkit.util.on('#modal-full', 'beforeshow', function () {
-
+        editor.on('change', function(event) {
+            document.getElementById('parsed-markdown-{{ $field->id }}').innerHTML = marked(editor.getValue());
+            document.getElementById('markdown-value-{{ $field->id }}').value = editor.getValue();
+        })
     });
 </script>
 @endpush
