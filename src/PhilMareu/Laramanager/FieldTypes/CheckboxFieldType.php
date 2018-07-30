528c452aa028e5fbbcbf4ac4b78b21cd991a4472
Phil Mareu <philmareu@gmail.com>
2018-07-30 17:38:14 -0500

    updated checkbox field

--- src/PhilMareu/Laramanager/FieldTypes/CheckboxFieldType.php
+++ src/PhilMareu/Laramanager/FieldTypes/CheckboxFieldType.php
@@ -3,7 +3,14 @@
 namespace PhilMareu\Laramanager\FieldTypes;
 
 
-class CheckboxFieldType
-{
+use Illuminate\Http\Request;
 
+class CheckboxFieldType extends FieldType
+{
+    public function mutate(Request $request, $name)
+    {
+        $request->offsetSet($name, $request->has($name));
+
+        return $request;
+    }
 }
