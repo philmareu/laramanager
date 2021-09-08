c3d422dbce856b0c000426d14ee14e1b0aa80d84
Phil Mareu <philmareu@gmail.com>
2021-09-08 08:23:22 -0500

    fix width issue due to UIkit update

--- src/views/layouts/admin/auth.blade.php
+++ src/views/layouts/admin/auth.blade.php
@@ -5,42 +5,45 @@
 @endsection
 
 @section('content')
-    <div class="uk-container uk-padding uk-text-center uk-width-1-1 uk-width-1-2@s uk-width-1-3@m uk-position-center">
+    <div class="uk-padding uk-text-center uk-position-center">
 
-        <div class="uk-text-large uk-margin uk-light">{{ config('app.name') }} Admin</div>
+        <div class="uk-width-large">
+            <div class="uk-text-large uk-margin uk-light">{{ config('app.name') }} Admin</div>
 
-        @if(session()->has('success'))
-            <div class="uk-alert uk-alert-success" data-uk-alert>
-                <a href="#" class="uk-alert-close uk-close"></a>
-                {{ session('success') }}
+            @if(session()->has('success'))
+                <div class="uk-alert uk-alert-success" data-uk-alert>
+                    <a href="#" class="uk-alert-close uk-close"></a>
+                    {{ session('success') }}
+                </div>
+            @elseif(session()->has('failed'))
+                <div class="uk-alert uk-alert-danger" data-uk-alert>
+                    <a href="#" class="uk-alert-close uk-close"></a>
+                    {{ session('failed') }}
+                </div>
+            @elseif(session()->has('status'))
+                <div class="uk-alert uk-alert-warning" data-uk-alert>
+                    <a href="#" class="uk-alert-close uk-close"></a>
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <div class="uk-card uk-card-default uk-card-small">
+
+                <div class="uk-card-header uk-text-left">
+                    <h3 class="uk-card-title">@yield('title')</h3>
+                </div>
+
+                <div class="uk-card-body">
+
+                    @yield('auth-content')
+
+                </div>
+
             </div>
-        @elseif(session()->has('failed'))
-            <div class="uk-alert uk-alert-danger" data-uk-alert>
-                <a href="#" class="uk-alert-close uk-close"></a>
-                {{ session('failed') }}
-            </div>
-        @elseif(session()->has('status'))
-            <div class="uk-alert uk-alert-warning" data-uk-alert>
-                <a href="#" class="uk-alert-close uk-close"></a>
-                {{ session('status') }}
-            </div>
-        @endif
 
-        <div class="uk-card uk-card-default uk-card-small">
-
-            <div class="uk-card-header uk-text-left">
-                <h3 class="uk-card-title">@yield('title')</h3>
-            </div>
-
-            <div class="uk-card-body">
-
-                @yield('auth-content')
-
-            </div>
+            <div class="uk-light uk-margin">Laramanager by <a href="https://philmareu.com" class="uk-link-muted">Phil Mareu</a></div>
 
         </div>
-
-        <div class="uk-light uk-margin">Laramanager by <a href="https://philmareu.com" class="uk-link-muted">Phil Mareu</a></div>
 
     </div>
 @endsection
