a0275ac684b0edcb09ff2975a5f3c4895b2a761e
Phil Mareu <philmareu@gmail.com>
2021-09-10 11:10:55 -0500

    fix editor resource location and config

--- src/views/objects/core/wysiwyg/scripts.blade.php
+++ src/views/objects/core/wysiwyg/scripts.blade.php
@@ -1,8 +1,54 @@
-<script src="{{ asset('vendor/laramanager/js/ckeditor/ckeditor.js') }}"></script>
+<script src="{{ asset('vendor/laramanager/js/ckeditor.js') }}"></script>
 
 <script>
-    CKEDITOR.replace('editor', {
-        customConfig: '/vendor/laramanager/js/ckeditor.js',
-        filebrowserImageBrowseUrl: SITE_URL + '/admin/images/browser'
-    });
+    ClassicEditor
+        .create( document.querySelector( '#editor' ), {
+
+            toolbar: {
+                items: [
+                    'heading',
+                    '|',
+                    'bold',
+                    'italic',
+                    'link',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'alignment',
+                    'imageInsert',
+                    'blockQuote',
+                    'insertTable',
+                    'mediaEmbed',
+                    'undo',
+                    'redo'
+                ]
+            },
+            language: 'en',
+            image: {
+                toolbar: [
+                    'imageTextAlternative',
+                    'imageStyle:inline',
+                    'imageStyle:block',
+                    'imageStyle:side',
+                    '|',
+                    'toggleImageCaption',
+                ]
+            },
+            table: {
+                contentToolbar: [
+                    'tableColumn',
+                    'tableRow',
+                    'mergeTableCells'
+                ]
+            }
+        } )
+        .then( editor => {
+            window.editor = editor;
+        } )
+        .catch( error => {
+            console.error( 'Oops, something went wrong!' );
+            console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
+            console.warn( 'Build id: 6b9ttjvwl5da-903apon27xyn' );
+            console.error( error );
+        } );
 </script>
